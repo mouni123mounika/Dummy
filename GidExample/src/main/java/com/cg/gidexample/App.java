f34e646873d11e5a29b472675597fef3be08490b
learning <learning@DIN66007240.corp.capgemini.com>
2018-07-10 10:22:58 +0530

    version 1.0-Mounika

---
+++ GidExample/src/main/java/com/cg/gidexample/App.java
@@ -0,0 +1,13 @@
+package com.cg.gidexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Mounika" );
+    }
+}
